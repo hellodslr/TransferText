315ce3e0b12791c4910cf0e1f412938ff658c9b2
hellodslr <hellodslr@gmail.com>
2020-10-16 21:02:04 +0900

    TransferText

---
+++ app/src/main/java/com/example/transfertext/Main2Activity.java
@@ -0,0 +1,34 @@
+package com.example.transfertext;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button Back = findViewById(R.id.bt_back);
+        TextView Display = findViewById(R.id.textView);
+        Bundle bn = getIntent().getExtras();
+        assert bn != null;
+        String name = bn.getString("abc");
+        Display.setText(String.valueOf(name));
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/transfertext/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.transfertext;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText inputText = findViewById(R.id.editText);
+        final TextView outputText = findViewById(R.id.textView);
+        Button Add = findViewById(R.id.bt_add);
+        Button Save = findViewById(R.id.bt_save);
+
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Value = inputText.getText().toString();
+                outputText.setText(Value);
+            }
+        });
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Data = outputText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("abc", Data);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
