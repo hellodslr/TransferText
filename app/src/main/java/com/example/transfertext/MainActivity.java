d3503be5585e514575ff0b7a3a523805d719a3eb
hellodslr <hellodslr@gmail.com>
2020-10-19 20:12:04 +0900

    Button if

--- app/src/main/java/com/example/transfertext/MainActivity.java
+++ app/src/main/java/com/example/transfertext/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.transfertext;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,14 +35,23 @@ public class MainActivity extends AppCompatActivity {
                 outputText.setText(Value);
             }
         });
+
         Save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Data = outputText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                intent.putExtra("data", Data);
-                startActivity(intent);
-                finish();
+                if (TextUtils.isEmpty(outputText.getText().toString())) {
+                    String Data = inputText.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    intent.putExtra("data", Data);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    String Data = outputText.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    intent.putExtra("data", Data);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
